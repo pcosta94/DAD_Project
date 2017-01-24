d4eea99ce0f7d9233344518bce7643f8e4d122c0
pcosta94 <pcdungeon24@gmail.com>
2017-01-24 14:35:59 +0000

    app.ts

---
+++ node/app.ts
@@ -0,0 +1,56 @@
+const restify = require('restify');
+const passport = require('passport');
+const path = require('path');
+
+import {databaseConnection as database} from './app.database';
+import {WebSocketServer} from './app.websockets';
+import {HandlerSettings} from './handler.settings';
+import io = require('socket.io-client');
+
+const url = 'mongodb://localhost:27017/sueca';
+
+// Create Restify and WebSocket Server
+const restifyServer = restify.createServer();
+const socketServer = new WebSocketServer();
+
+// Prepare and configure Restify Server
+restifyServer.use(restify.CORS());
+restify.CORS.ALLOW_HEADERS.push("content-type");
+restify.CORS.ALLOW_HEADERS.push("authorization");
+restifyServer.use(restify.bodyParser());
+restifyServer.use(restify.queryParser());
+
+restifyServer.use(restify.fullResponse());
+
+
+// Prepare and configure Passport based security
+import {Security} from './app.security';
+let security = new Security();
+security.initMiddleware(restifyServer);
+
+// Settings are used on all HTTP (Restify) Handlers
+let settings = new HandlerSettings(socketServer, security,'/api/v1/');
+
+// Authentication Handlers
+import {Authentication} from './app.authentication';
+new Authentication().init(restifyServer, settings);
+
+
+// Players Handler
+import {Player} from './app.players';
+new Player().init(restifyServer, settings);
+
+// Games Handler
+import {Game} from './app.games';
+new Game().init(restifyServer, settings);
+
+restifyServer.get(/^\/(?!api\/).*/, restify.serveStatic({
+  directory: '../angular',
+  default: 'index.html'
+}));
+
+database.connect(url, () => {
+    restifyServer.listen(7777, () => console.log('%s listening at %s', restifyServer.name, restifyServer.url));
+    // Websocket is initialized after the server
+    socketServer.init(restifyServer.server);
+});
